4217ec18478b8176e396a30688303024bd23c834
chinaluoyanbin <luoyanbin@live.cn>
2018-08-22 22:18:10 +0800

    Update All

--- experiment/plot.py
+++ experiment/plot.py
@@ -5,15 +5,15 @@ import matplotlib.pyplot as plt
 
 def plot_feature_importance(path):
     featureImportance = pd.read_csv(path, header=None)
-    # features = featureImportance.iloc[:, 0]
-    # importanceScore = featureImportance.iloc[:, 1]
+    features = featureImportance.iloc[:, 0]
+    importanceScore = featureImportance.iloc[:, 1]
 
     print(list(featureImportance.iloc[-19:, 0]))
     # print(featureImportance)
 
-    # f, ax = plt.subplots(figsize=(6, 15))
-    # sns.barplot(x=importanceScore, y=features)
-    # plt.show()
+    f, ax = plt.subplots(figsize=(20, 5))
+    sns.barplot(x=features, y=importanceScore)
+    plt.show()
 
     # plt.figure(figsize=(16, 4))
     # plt.bar(x, y)
@@ -25,11 +25,3 @@ def plot_feature_importance(path):
 
 if __name__ == '__main__':
     plot_feature_importance(path='./gbdt_featureImportance.csv')
-
-    # 删除19个
-    drop_columns = [
-        'u26', 'm21', 'on_u6', '4', '7', 'on_u8', 'u21', 'u31', '2', '6',
-        'u16', '1', 'u33', 'u32', '5', '3', 'on_u10', 'on_u12'
-        'on_u9'
-    ]
-    prediction(model='gbdt', drop_columns=drop_columns)
